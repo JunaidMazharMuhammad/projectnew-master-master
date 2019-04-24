310128ea2d57ac8039aec25a6f7f149665feca2e
Junaid <junaid.98@live.com>
2019-04-24 15:39:26 +0500

    Initial commit

---
+++ lib/Compass.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'dart:math'as math;
+
+class Compass extends StatefulWidget {
+  @override
+  _CompassState createState() => _CompassState();
+}
+
+class _CompassState extends State<Compass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
